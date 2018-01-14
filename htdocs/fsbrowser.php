263ed6e2693caacee937d227d734ff6ab4dce116
Craft4Cube <cubecraft15@gmail.com>
2018-01-14 21:31:30 +0100

    Fixed IOTD.... again
    Forgot to remove the <style> tag before the script.

--- htdocs/fsbrowser.php
+++ htdocs/fsbrowser.php
@@ -4,7 +4,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<title>FS Browser 1.0</title>
-		<style>
 		<?php
 			error_reporting(0);
 			ini_set('display_errors', 0);
